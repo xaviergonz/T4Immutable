5202c5ed1ddb0ee9ec351ca633b6585b89a0ea8b
xaviergonz <xaviergonz@gmail.com>
2016-11-06 03:45:26 +0100

    fixed issue with generics inside generics

---
+++ src/T4Immutable/GeneratedCodeAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace T4Immutable
+{
+  [AttributeUsage(AttributeTargets.All, AllowMultiple = true, Inherited = false)]
+  public sealed class GeneratedCodeAttribute : Attribute
+  {
+  }
+}
